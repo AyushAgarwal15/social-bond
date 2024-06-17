de67cc4f8ece2d20a6193d976ace6b7cb00f84ce
Ayush Agarwal <ayushagarwal8126@gmail.com>
2024-06-17 20:29:38 +0530

    Resolved explore tab error

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -34,13 +34,13 @@ const GridPostList = ({
               <div className="flex items-center justify-start gap-2 flex-1">
                 <img
                   src={
-                    post.creator.imageUrl ||
+                    post?.creator?.imageUrl ||
                     "/assets/icons/profile-placeholder.svg"
                   }
                   alt="creator"
                   className="w-8 h-8 rounded-full"
                 />
-                <p className="line-clamp-1">{post.creator.name}</p>
+                <p className="line-clamp-1">{post?.creator?.name}</p>
               </div>
             )}
             {showStats && <PostStats post={post} userId={user.id} />}
