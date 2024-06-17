f6b47075aaa0a5ac0acea9d7580a4c9a9a459b2a
Ayush Agarwal <ayushagarwal8126@gmail.com>
2024-06-17 20:30:16 +0530

    resolved explore tab issue

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ import "./globals.css";
 
 const App = () => {
   return (
-    <main className="flex h-screen">
+    <main className="flex">
       <Routes>
         {/* public routes */}
         <Route element={<AuthLayout />}>
